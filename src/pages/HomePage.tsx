4a3d76c3ef4d98c424fb7395957e726857e0eee4
richard28_dev <richardsajith11229@gmail.com>
2026-09-16 10:02:57 +0530

    Remove 3D architectural visualization section from homepage and streamline performance

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,6 @@ import HeroSection from '../components/home/HeroSection';
 import TrustStats from '../components/home/TrustStats';
 import FeaturedSection from '../components/home/FeaturedSection';
 import CuratedCollections from '../components/home/CuratedCollections';
-import Architectural3DShowcase from '../components/home/Architectural3DShowcase';
 import MarketTrendsSection from '../components/home/MarketTrendsSection';
 import MortgageCalculatorSection from '../components/home/MortgageCalculatorSection';
 import ServicesSection from '../components/home/ServicesSection';
@@ -18,7 +17,6 @@ export default function HomePage() {
       <TrustStats />
       <FeaturedSection />
       <CuratedCollections />
-      <Architectural3DShowcase />
       <MarketTrendsSection />
       <MortgageCalculatorSection />
       <ServicesSection />
